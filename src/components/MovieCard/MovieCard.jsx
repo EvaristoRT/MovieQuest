3042f911eec17425c40187bfa42949092e64d26b
Evaristo Romero Tirado <romeroevaristo2005@gmail.com>
2026-07-28 19:44:17 -0600

    fix(movie-card): add fallback poster placeholder

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,13 +1,25 @@
 import { GoDotFill } from "react-icons/go";
 import { FaStar } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import moviePosterPlaceHolder from "../../assets/images/moviePosterPlaceHolder.png"
+import moviePosterPlaceHolderLight from "../../assets/images/moviePosterPlaceHolderLight.png"
 import "./MovieCard.css"
+import { useContext } from "react";
+import { ThemeContext } from "../../context/ThemeContext";
+import { IMAGE_BASE_URL } from "../../services/tmdb";
 function MovieCard({ id, image, rate, title, genre, year }) {
+    const {darkMode} = useContext(ThemeContext)
     return (
         <Link to={`/movie/${id}`} className="movie-card">
             <article>
                 <div className="movie-card__top" style={{
-                    backgroundImage: `url(${image})`,
+                    backgroundImage: `url(${
+                        image !== IMAGE_BASE_URL+"null"
+                            ? image
+                            : darkMode
+                                ? moviePosterPlaceHolder
+                                : moviePosterPlaceHolderLight
+                    })`,
                     backgroundSize: "cover",
                     backgroundPosition: "center",
                     backgroundRepeat: "no-repeat",
